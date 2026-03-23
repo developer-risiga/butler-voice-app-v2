8335f6c6312add42a8594bdb42d0cb122966e4e8
SVS Kethan Reddy <sai@risiga.com>
2026-03-23 23:45:40 +0530

    Add GoTrue installation to SupabaseClient

--- app/src/main/java/com/demo/butler_voice_app/api/SupabaseClient.kt
+++ app/src/main/java/com/demo/butler_voice_app/api/SupabaseClient.kt
@@ -2,14 +2,15 @@ package com.demo.butler_voice_app.api
 
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.gotrue.GoTrue
 import io.github.jan.supabase.postgrest.Postgrest
 
 object SupabaseClient {
-
     val client: SupabaseClient = createSupabaseClient(
         supabaseUrl = "https://dcabhsrchagikwzjmbvj.supabase.co",
         supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjYWJoc3JjaGFnaWt3emptYnZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0MjUwMTQsImV4cCI6MjA4ODAwMTAxNH0.h6v1GefgjWt_hlwzVgqcuH-eYJ1cK-I9RuP48MSdfCs"
     ) {
         install(Postgrest)
+        install(GoTrue)
     }
 }
