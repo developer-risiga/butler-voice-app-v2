649c40bbaf94d236ecaa8d8a9dfc06fbb5afb1db
SVS Kethan Reddy <sai@risiga.com>
2026-03-24 00:43:46 +0530

    Refactor SupabaseClient by removing Auth installation
    Removed Auth installation from SupabaseClient and added constants for Supabase URL and key.

--- app/src/main/java/com/demo/butler_voice_app/api/SupabaseClient.kt
+++ app/src/main/java/com/demo/butler_voice_app/api/SupabaseClient.kt
@@ -2,7 +2,6 @@ package com.demo.butler_voice_app.api
 
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.SupabaseClient
-import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.postgrest.Postgrest
 
 object SupabaseClient {
@@ -11,6 +10,8 @@ object SupabaseClient {
         supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjYWJoc3JjaGFnaWt3emptYnZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0MjUwMTQsImV4cCI6MjA4ODAwMTAxNH0.h6v1GefgjWt_hlwzVgqcuH-eYJ1cK-I9RuP48MSdfCs"
     ) {
         install(Postgrest)
-        install(Auth)
     }
+
+    const val SUPABASE_URL = "https://dcabhsrchagikwzjmbvj.supabase.co"
+    const val SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjYWJoc3JjaGFnaWt3emptYnZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0MjUwMTQsImV4cCI6MjA4ODAwMTAxNH0.h6v1GefgjWt_hlwzVgqcuH-eYJ1cK-I9RuP48MSdfCs"
 }
