fba2c506cb07517fbb920e69ef346f936a96979d
SVS Kethan Reddy <sai@risiga.com>
2026-03-24 02:20:39 +0530

    Remove Supabase client setup from SupabaseClient.kt
    Removed Supabase client initialization and imports.

--- app/src/main/java/com/demo/butler_voice_app/api/SupabaseClient.kt
+++ app/src/main/java/com/demo/butler_voice_app/api/SupabaseClient.kt
@@ -1,17 +1,6 @@
 package com.demo.butler_voice_app.api
 
-import io.github.jan.supabase.createSupabaseClient
-import io.github.jan.supabase.SupabaseClient
-import io.github.jan.supabase.postgrest.Postgrest
-
 object SupabaseClient {
-    val client: SupabaseClient = createSupabaseClient(
-        supabaseUrl = "https://dcabhsrchagikwzjmbvj.supabase.co",
-        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjYWJoc3JjaGFnaWt3emptYnZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0MjUwMTQsImV4cCI6MjA4ODAwMTAxNH0.h6v1GefgjWt_hlwzVgqcuH-eYJ1cK-I9RuP48MSdfCs"
-    ) {
-        install(Postgrest)
-    }
-
     const val SUPABASE_URL = "https://dcabhsrchagikwzjmbvj.supabase.co"
     const val SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjYWJoc3JjaGFnaWt3emptYnZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0MjUwMTQsImV4cCI6MjA4ODAwMTAxNH0.h6v1GefgjWt_hlwzVgqcuH-eYJ1cK-I9RuP48MSdfCs"
 }
