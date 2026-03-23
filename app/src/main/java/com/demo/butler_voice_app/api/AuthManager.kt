d41965251965b5d34ca8c6bd79ce8856b889b857
SVS Kethan Reddy <sai@risiga.com>
2026-03-24 00:24:24 +0530

    Update Supabase imports in AuthManager.kt

--- app/src/main/java/com/demo/butler_voice_app/api/AuthManager.kt
+++ app/src/main/java/com/demo/butler_voice_app/api/AuthManager.kt
@@ -1,7 +1,7 @@
 package com.demo.butler_voice_app.api
 
-import io.github.jan.supabase.auth.auth
-import io.github.jan.supabase.auth.providers.builtin.Email
+import io.github.jan.supabase.gotrue.auth
+import io.github.jan.supabase.gotrue.providers.builtin.Email
 
 object AuthManager {
 
